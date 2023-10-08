3c56cfd95f4dc0a76b305e73f68a1589b0154323
sachinyadav <sachinyadav111140@gmail.com>
2023-10-08 23:47:46 +0530

    First Commit

---
+++ src/main/java/com/example/CiCd/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.example.CiCd.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class MainController {
+
+    @GetMapping( "/hello")
+    public ResponseEntity<String> getHello()
+    {
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
+}
